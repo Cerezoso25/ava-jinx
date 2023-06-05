769c3da22685a9fc0b0881369b2b40a354cbf04c
Cerezoso25 <135590634+Cerezoso25@users.noreply.github.com>
2023-06-05 09:53:58 +0000

    Update ava-jinx.sh

--- ava-jinx.sh
+++ ava-jinx.sh
@@ -21,7 +21,7 @@ rmdir /home/${USER}/.local/share/Ryujinx-Ava/publish
 # download the icon
 echo "Downloading Icon..."
 mkdir -p /home/${USER}/.local/share/icons
-curl -s -L "https://raw.githubusercontent.com/Cerezoso/ava-jinx/main/Ryujinx.png" > Ryujinx.png > /home/${USER}/.local/share/icons/Ryujinx.png
+curl -L "https://raw.githubusercontent.com/edisionnano/Pine-jinx/main/Ryujinx.png" > /home/${USER}/.local/share/icons/Ryujinx.png
 
 # remove the downloaded tarball
 rm test-ava-ryujinx-${version}-linux_x64.tar.gz
